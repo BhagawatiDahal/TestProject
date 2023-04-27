c02ab5ac9803630b7e912af250d713904b5cd1ba
BhagawatiDahal <brave.bani@gmail.com>
2023-04-26 19:41:50 -0500

    new File commited

---
+++ src/gitProject12/Test11.java
@@ -0,0 +1,21 @@
+package gitProject12;
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	String []	names= {"Bhagawati", "Sampada", "Anjila"};
+	
+		for(int i=0; i<=names.length-1; i++) {
+			System.out.println(names[i]);
+		}
+		
+		
+		
+		
+	}
+
+}
